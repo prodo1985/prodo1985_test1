0994a8a7698828448206cb9bbe213b6b0cc31fda
prodo1985 <61434588+prodo1985@users.noreply.github.com>
2020-03-28 23:46:06 +0500

    Add files via upload

---
+++ pidar.py
@@ -0,0 +1,35 @@
+import horoscope
+from datetime import datetime as dt
+
+def generate_body (header,paragraphs):
+    body="<h1>" + header + "</h1>"
+    for p in paragraphs:
+        body+="<p>"+ p + "</p>"
+    return "<body><center><font size='4em'>"+  body + "</center></body>"
+
+def generate_page(head,body):
+    page="<html>"+ head+body+"</html>"
+    return page
+
+def generate_head(title):
+    title="<title>"+ title+"</title>"
+    return "<head>"+title+"</head>"
+
+def save_page(title, header, paragraps, output="2.html"):
+    fp=open(output,'w')
+    today=dt.now().date()
+    page=generate_page(
+        head=generate_head(title),
+        body=generate_body(header, paragraps)
+    )
+    print(page, file=fp)
+
+    fp.close()
+
+body_page=generate_body(header="Гороскоп на 2018-11-12", paragraphs=horoscope.generate_prophecies())
+save_page("pidar","zalupa",horoscope.generate_prophecies())
+
+#fp=open("1.html","w")
+#print(body_page,file=fp)
+#fp.close()
+
